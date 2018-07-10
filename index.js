5a1a16697937c4b0f2043b31a32c348d19bf06c8
Ben Schwartz <bens2913@gmail.com>
2018-07-10 19:44:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,9 +12,10 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if (string === "hello") {
-    Return "I can\'t hear you!"
+    return "I can\'t hear you!"
   } else if (string === "HELLO") {
-    console.log("YES INDEED!")
+    return"YES INDEED!"
   } else if (string === "I love you, Grandma.") {
-  console.log("I love you, too.")}
+  return "I love you, too."
+  }
 }
